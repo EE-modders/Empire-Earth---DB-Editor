2913c4981040414c8fb8b7ed32537cf7162f185b
Unknown <zocker1600@posteo.net>
2020-01-15 00:40:01 +0100

    release 2.1c

--- src/datmanager/Settings.java
+++ src/datmanager/Settings.java
@@ -8,7 +8,7 @@ package datmanager;
 public class Settings {
 
 	/** The editor version */
-	public static final String VERSION = "2.1b";
+	public static final String VERSION = "2.1c";
 	
 	/** The editor database version */
 	public static final String DBVersion = "2020.01.14";
