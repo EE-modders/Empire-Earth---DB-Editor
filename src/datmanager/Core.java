c591684036a9b46119788006000bc2b992f0d79c
zocker-160 <zocker1600@posteo.net>
2020-03-30 18:01:06 +0200

    check java version on startup

--- src/datmanager/Core.java
+++ src/datmanager/Core.java
@@ -21,8 +21,15 @@ public class Core {
 
 	private static final String[] editorModeChoices = new String[] { "Vanilla", "Art of Conquest", "Exit"};
 	private static final String titleText = "Empire Earth - DB Editor";
-	private static final String popupText = "v" + Settings.VERSION + "\n" + "database version: " + Settings.DBVersion + "\n\n" + "Vanilla or AOC?";
-
+	
+	private static final String editorVersion = "v" + Settings.VERSION + "\n";
+	private static final String databaseVersion = "database version: " + Settings.DBVersion + "\n";
+	private static final String javaVersion = "java version: " + System.getProperty("java.version") + "\n";	
+	private static final String questionText = "\n" + "Vanilla or AoC?";
+	
+	
+	private static final String popupText = editorVersion + databaseVersion + javaVersion + questionText; 
+	
 	/** If true, the editor is in AOC mode */
 	private static boolean AOC = false;
 
@@ -32,19 +39,23 @@ public class Core {
 	 * Check if the editor is in AOC mode
 	 *
 	 * @return true if editor is in AOC mode, false otherwise
-	 */
-	public static boolean isAOC() { return AOC; }
-
+	 */	
 	private static final File DataDirectory = Paths.get("EEEditorData").toFile();
 	
 	public static File getDataDirectory() { return new File(DataDirectory, Core.isAOC() ? "AOC" : "Vanilla"); }
-
 	public static String getGameDirectory() { return gameDirectory; }
-
+	public static boolean isAOC() { return AOC; }
+	
+	
 	public static void main(String[] args) {
 		final EESplashScreen splashScreen = new EESplashScreen();
 		splashScreen.setVisible(true);
 
+		if (!supportedJava()) {
+			JOptionPane.showMessageDialog(null, "You are using a not supported version of java! \n\n this can cause problems and bugs \n please consider updating to java 11 or up", "warning", JOptionPane.WARNING_MESSAGE);
+			//System.exit(0);
+		}
+		
 		switch (JOptionPane.showOptionDialog(splashScreen, popupText, titleText, 0, JOptionPane.QUESTION_MESSAGE, null, editorModeChoices, editorModeChoices[0])) {
 			case JOptionPane.CLOSED_OPTION:
 			case 0:
@@ -78,6 +89,25 @@ public class Core {
 		FrameMain.instance.setVisible(true);
 	}
 
+	private static boolean supportedJava() {
+		String version = System.getProperty("java.version");
+		int nVersion;
+		
+		if (version.startsWith("1.")) {
+		    version = version.substring(2, 3);
+		} else {
+		    int dot = version.indexOf(".");
+		    if (dot != -1) { version = version.substring(0, dot); }
+		}
+		nVersion = Integer.parseInt(version);
+		
+		if (nVersion < 11) {
+			return false;
+		}else {
+			return true;
+		}
+	}
+	
 	private static void readGameFolder() {
 		final String location = AOC ? "HKCU\\Software\\Mad Doc Software\\EE-AOC" : "HKCU\\Software\\SSSI\\Empire Earth";
 		try {
