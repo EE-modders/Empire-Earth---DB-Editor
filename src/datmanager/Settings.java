1077efa01de0d67abcf9a83e74d532313932a132
zocker-160 <zocker1600@posteo.net>
2020-03-30 18:05:33 +0200

    fix version number

--- src/datmanager/Settings.java
+++ src/datmanager/Settings.java
@@ -8,7 +8,7 @@ package datmanager;
 public class Settings {
 
 	/** The editor version */
-	public static final String VERSION = "2.2.4";
+	public static final String VERSION = "2.1.4";
 	
 	/** The editor database version */
 	public static final String DBVersion = "2020.03.30";
