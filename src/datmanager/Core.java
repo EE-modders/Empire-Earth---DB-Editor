723d5e0f1d466e57d8e85a5015d7db131e578388
zocker-160 <zocker1600@posteo.net>
2022-02-13 01:57:56 +0100

    Core refactor

--- src/datmanager/Core.java
+++ src/datmanager/Core.java
@@ -3,8 +3,6 @@ package datmanager;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
-import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import javax.swing.JOptionPane;
 
@@ -19,46 +17,42 @@ import gui.FrameMain;
  */
 public class Core {
 
-	private static final String[] editorModeChoices = new String[] { "Vanilla", "Art of Conquest", "Exit"};
-	private static final String titleText = "Empire Earth - DB Editor";
+	public static final String titleText = "Empire Earth - DB Editor";
+	private static final String[] editorModeChoices = new String[] { "EE Classic", "Art of Conquest", "Exit" };
 	
 	private static final String editorVersion = "v" + Settings.VERSION + "\n";
-	private static final String v_databaseVersion = "Vanilla database version: " + Settings.base_DBVersion + "\n";
+	private static final String v_databaseVersion = "EEC database version: " + Settings.base_DBVersion + "\n";
 	private static final String aoc_databaseVersion = "AoC database version: " + Settings.AoC_DBVersion + "\n";
-	private static final String javaVersion = "java version: " + System.getProperty("java.version") + "\n";	
-	private static final String questionText = "\n" + "Vanilla or AoC?";
-	
-	
+	private static final String javaVersion = "Java version: " + System.getProperty("java.version") + "\n";
+	private static final String questionText = "\nEEC or AoC?";
+
 	private static final String popupText = editorVersion + v_databaseVersion + aoc_databaseVersion + javaVersion + questionText;
 	
 	/** If true, the editor is in AOC mode */
 	private static boolean AOC = false;
-
-	private static String gameDirectory = null;
-
+	private static final File dataDirectory = Paths.get("EEEditorData").toFile();
+	
+	public static File getDataDirectory() { return new File(dataDirectory, Core.isAOC() ? "AOC" : "Vanilla"); }
 	/**
 	 * Check if the editor is in AOC mode
 	 *
 	 * @return true if editor is in AOC mode, false otherwise
-	 */	
-	private static final File DataDirectory = Paths.get("EEEditorData").toFile();
-	
-	public static File getDataDirectory() { return new File(DataDirectory, Core.isAOC() ? "AOC" : "Vanilla"); }
-	public static String getGameDirectory() { return gameDirectory; }
+	 */
 	public static boolean isAOC() { return AOC; }
-	
-	
+
 	public static void main(String[] args) {
 		final EESplashScreen splashScreen = new EESplashScreen();
 		splashScreen.setVisible(true);
 
 		if (!supportedJava()) {
-			JOptionPane.showMessageDialog(null, "You are using a not supported version of java! \n\n this can cause problems and bugs \n please consider updating to java 11 or up", "warning", JOptionPane.WARNING_MESSAGE);
-			//System.exit(0);
+			JOptionPane.showMessageDialog(splashScreen,
+					"You are using a not supported version of Java!\n\n"
+							+"this can cause problems and bugs \n"
+							+"please consider updating to Java 11 or up",
+					"Warning", JOptionPane.WARNING_MESSAGE);
 		}
-		
-		switch (JOptionPane.showOptionDialog(splashScreen, popupText, titleText, 0, JOptionPane.QUESTION_MESSAGE, null, editorModeChoices, editorModeChoices[0])) {
-			case JOptionPane.CLOSED_OPTION:
+
+		switch (JOptionPane.showOptionDialog(splashScreen, popupText, titleText, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, editorModeChoices, editorModeChoices[0])) {
 			case 0:
 				AOC = false;
 				break;
@@ -66,24 +60,23 @@ public class Core {
 				AOC = true;
 				break;
 			case 2:
+			case JOptionPane.CLOSED_OPTION:
 				System.exit(0);
 				break;
 		}
 
-		final Thread registryThread = new Thread(Core::readGameFolder);
-		final Thread languages = new Thread(Language::getList); // This makes the Language class initialize in background... SSSHHH!!!
-		final Thread datStructures = new Thread(DatStructure::initAllStructures);
-		registryThread.start();
-		languages.start();
-		datStructures.start();
+		final var languageThread = new Thread(Language::updateLanguages); // This makes the Language class initialize in background... SSSHHH!!!
+		final var datStructuresThread = new Thread(DatStructure::initAllStructures);
+
+		languageThread.start();
+		datStructuresThread.start();
 
 		try {
-			registryThread.join();
-			languages.join();
-			datStructures.join();
+			languageThread.join();
+			datStructuresThread.join();
 		} catch (final InterruptedException e) {
 			Util.printException(splashScreen, e, true);
-			return;
+			System.exit(0);
 		}
 
 		splashScreen.setVisible(false);
@@ -92,7 +85,6 @@ public class Core {
 
 	private static boolean supportedJava() {
 		String version = System.getProperty("java.version");
-		int nVersion;
 		
 		if (version.startsWith("1.")) {
 		    version = version.substring(2, 3);
@@ -100,39 +92,40 @@ public class Core {
 		    int dot = version.indexOf(".");
 		    if (dot != -1) { version = version.substring(0, dot); }
 		}
-		nVersion = Integer.parseInt(version);
-		
-		if (nVersion < 11) {
-			return false;
-		}else {
-			return true;
-		}
+
+		return Integer.parseInt(version) >= 11;
 	}
-	
+
+	/*
+	// Some Windows bullshit nobody cares about
 	private static void readGameFolder() {
+		if (System.getProperty("os.name").equalsIgnoreCase("Linux")) {
+			// There is no registry on Linux, so just ignore
+			System.out.println("Linux found, ignoring registry lookup.");
+			return;
+		}
 		final String location = AOC ? "HKCU\\Software\\Mad Doc Software\\EE-AOC" : "HKCU\\Software\\SSSI\\Empire Earth";
 		try {
 			final Process processVolume = readRegistry(location, "Installed From Volume");
 			final Process processDirectory = readRegistry(location, "Installed From Directory");
 			processVolume.waitFor();
 			processDirectory.waitFor();
+
 			try (Scanner volumeScanner = new Scanner(processVolume.getInputStream());
-					Scanner directoryScanner = new Scanner(processDirectory.getInputStream())) {
+				 Scanner directoryScanner = new Scanner(processDirectory.getInputStream())) {
 				final var volume = volumeScanner.tokens().collect(Collectors.joining(" "));
 				final var directory = directoryScanner.tokens().collect(Collectors.joining(" "));
+
 				gameDirectory = volume.substring(volume.indexOf("REG_SZ") + 6).strip() + directory.substring(directory.indexOf("REG_SZ") + 6).strip();
 				System.out.println("Found game base directory: " + gameDirectory);
 			}
-		} catch (final IOException | InterruptedException exc) {
+		} catch (final IOException | InterruptedException e) {
+			System.out.println("Could not find game base directory using registry.");
 		}
 	}
 
 	private static Process readRegistry(String location, String key) throws IOException {
 		return Runtime.getRuntime().exec("reg query \"" + location + "\" /v \"" + key + "\"");
 	}
-
-	/** No need to instantiate this */
-	private Core() {
-	}
-
+	 */
 }
