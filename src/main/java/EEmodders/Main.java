b9df1d2795d2009ee53c30025d4b66640708e361
zocker-160 <zocker1600@posteo.net>
2022-07-30 19:37:54 +0200

    add proper Main class and fix folder structure

---
+++ src/main/java/EEmodders/Main.java
@@ -0,0 +1,10 @@
+package EEmodders;
+
+import EEmodders.datmanager.Core;
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(Core.class, args);
+    }
+}
