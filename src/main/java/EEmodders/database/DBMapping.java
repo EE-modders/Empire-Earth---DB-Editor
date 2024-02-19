fee08840e8779838d9cca767ddaa0dd7b351ec1e
zocker-160 <zocker1600@posteo.net>
2024-02-19 13:25:13 +0100

    add dbcliffterrain

--- src/main/java/EEmodders/database/DBMapping.java
+++ src/main/java/EEmodders/database/DBMapping.java
@@ -26,7 +26,8 @@ public class DBMapping {
             //Map.entry("Area Effect Table", "dbareaeffect.dat")
             //Map.entry("Buttons", "dbbuttons.dat")
             //Map.entry("Calamity", "dbcalamity.dat")
-            Map.entry("Civilization", "dbcivilization.dat")
+            //Map.entry("Civilization", "dbcivilization.dat")
+            Map.entry("Cliff Terrain", "dbcliffterrain.dat")
     );
 
     private static final Map<String, String> EE_MAPPING = Map.ofEntries(
@@ -38,8 +39,8 @@ public class DBMapping {
             Map.entry("Buttons", "buttonsdata.dat"),
             Map.entry("Calamity", "calamity.dat"),
             Map.entry("Civilization", "civilization.dat"),
-
             Map.entry("Cliff Terrain", "cliff.dat"),
+
             Map.entry("ColorTable", "colortabledata.dat"),
             Map.entry("Control Events", "uicontrolevents.dat"),
             Map.entry("Effect", "effects.dat"),
